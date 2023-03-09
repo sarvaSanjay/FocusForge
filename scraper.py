40f30d3763c4f1a6753337740ab526ba72207d73
Sarva Sanjay <sarva.sanjay@mail.utoronto.ca>
2023-03-08 19:30:41 -0500

    Course names added

--- scraper.py
+++ scraper.py
@@ -6,9 +6,11 @@ soup = BeautifulSoup(r.text, 'html.parser')
 courses = soup.find_all("div", class_="no-break views-row")
 course = courses[0]
 course_soups = [BeautifulSoup(str(course), 'html.parser') for course in courses]
-course_names = [str(soup.find('h3')).strip()[4:-5] for soup in course_soups]
-course_code = [course.split('-')[0].strip() for course in course_names]
-with open('course_code.txt', 'w') as f:
-    for code in course_code:
-        f.write(code + '\n')
-print(course_code)
+course_titles = [str(soup.find('h3')).strip()[4:-5] for soup in course_soups]
+course_code = [course.split('-')[0].strip() for course in course_titles]
+course_names = [course.split('-')[1].strip() for course in course_titles]
+with open('course_names.txt', 'w') as f:
+    for name in course_names:
+        f.write(name + '\n')
+print(course_names[:100])
+print(len(course_code) == len(course_names))
