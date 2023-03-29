262f97b4329fcb7c5021d2511b98c547b032b575
raahilv <raahilv@gmail.com>
2023-03-29 14:11:10 -0400

    Add files via upload

--- main_question_mark.py
+++ main_question_mark.py
@@ -1,49 +1,58 @@
 from __future__ import annotations
+import math  # maybe we can work on a simplar solution later? Maybe not?
+
+
 class _Course():
     course_code: str
     name: str
-    # ERROR: At least read the proposal. The prereqs are list of sets of courses because some prereqs have multiple options. Like MAT137/ MAT157 but not both.
-    prereqs: set[_Course]
+    prereqs: list[set[_Course]]
     credits: float
 
-    def __init__(self, code: str, name: str, prereqs: set[_Course]):
+    def __init__(self, code: str, name: str, prereqs: list[set[_Course]]):
         self.course_code = code
         self.name = name
         self.prereqs = prereqs
-        # ERROR: Do 'Y1' instead of 'Y' cos course codes like PSY270H1 exist.
-        if 'Y' in self.course_code:
+        if self.course_code[6] == 'Y':  # Better solution. PSY129H1 would get assigned 1 with suggested solution
             self.credits = 1.0
         else:
             self.credits = 0.5
 
-    def get_prereqs(self) -> set[set[_Course]]:
+    def get_prereqs(self) -> list[set[_Course]]:  # can't have set in set
         # if course is CSC265, prereqs are CSC240 or CSC236 (excluding coreqs)
         # CSC240 prereqs are none
         # CSC236 prereqs are (CSC148 and CSC165) OR CSC(csc111)
         # so this methods returns {{CSC240}, {CSC236, CSC148, CSC165}, {CSC236, CSC111}}
-        return # nooooooo, it's too hard
+        return  # nooooooo, it's too hard
 
 
 class Focus():
     name: str
-    # ERROR: You cannot do set of set. Sets are unhashable. We need to work on this.
-    course_reqs = set[set[_Course]] #top  level prereqs
+    course_reqs = list[set[_Course]]  # top  level prereqs
     credits_req = float
-    program_code = str
+    program_code = str  # the long string of alphabets that nobody really cares about. like ASFOC1689F
 
     def __init__(self, name: str, courses_reqs: set[set[_Course]], credits_req: float, program_code: str):
         self.name = name
         self.course_reqs = courses_reqs
         self.credits_req = credits_req
-        # QUESTION: What exactly do you mean by program_code?
         self.program_code = program_code
 
     def credits_left(self, completed: set[_Course]) -> float:
-        total_preqreqs = []
+        """Returns the number of credits left to complete a focus"""
+        total_prereqs = [set()]
         for path in self.course_reqs:
-            temp_set = set()
+            temp_list = [set()]
             for course in path:
-                temp_set.add(course)
-            total_prereqs.append(path.
+                temp_list = cartesian_product(temp_list, course.get_prereqs())
+                temp_list = cartesian_product(temp_list, [{course}])
+            total_prereqs = cartesian_product(temp_list, total_prereqs)
+        min = math.inf
+        for path in total_prereqs:
+            if sum(course.credits for course in path if course not in completed) < min:
+                min = sum(course.credits for course in path)
+        return min
 
 
+def cartesian_product(lst1: list[set], lst2: list[set]) -> list[set]:
+    """Returns a cartesian product"""
+    return [set1.union(set2) for set1 in lst1 for set2 in lst2]
