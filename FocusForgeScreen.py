71a05a1f2337514aa458c5bc64567b51ce56e970
Anshiboy <ansh30704@gmail.com>
2023-04-02 22:45:49 -0400

    mm

--- FocusForgeScreen.py
+++ FocusForgeScreen.py
@@ -15,23 +15,6 @@ root.configure(background="#282634")
 listy = set()
 focy = ''
 
-def new_window_compvis():
-    window = Toplevel(root)
-    window.title("Focus Selector")
-    window.geometry("500x600")
-    window.configure(background="#282634")
-    label = Label(window, text="Focus Selector", bg="#282634", fg="white")
-    label.place(relx=0.5, rely=0.05, anchor=CENTER)
-
-    tree = ttk.Treeview(window, column=("c1", "c2", "c3"), show='headings', height=8)
-    tree.column("# 1", anchor=CENTER)
-    tree.heading("# 1", text="Year to take")
-    tree.column("# 2", anchor=CENTER)
-    tree.heading("# 2", text="Fall")
-    tree.column("# 3", anchor=CENTER)
-    tree.heading("# 3", text="Winter")
-    # Insert the data in Treeview widget
-
 
 # @check_contracts
 def new_window_tt():
@@ -68,29 +51,18 @@ def new_window_tt():
                     command= lambda: print_focus(dropdown1.get(), window=window))
     button.place(relx=0.5, rely=0.6, anchor=CENTER)
 
-
     button = Button(window, text="1", width=4, height=3, bg="#282620", fg="white",
                     command= lambda: find_all_subjects(dropdown1.get(),listy))
     button.place(relx=0.35, rely=0.75, anchor=CENTER)
     button = Button(window, text="2", width=4, height=3, bg="#282620", fg="white",
-                    command=lambda: new_window_compvis(dropdown1.get(), 2))
+                    command=lambda: find_all_subjects(dropdown1.get(),listy))
     button.place(relx=0.45, rely=0.75, anchor=CENTER)
     button = Button(window, text="3", width=4, height=3, bg="#282620", fg="white",
-                    command=lambda: new_window_compvis(dropdown1.get(), 3))
+                    command=lambda: find_all_subjects(dropdown1.get(),listy))
     button.place(relx=0.55, rely=0.75, anchor=CENTER)
     button = Button(window, text="4", width=4, height=3, bg="#282620", fg="white",
-                    command= lambda: new_window_compvis(dropdown1.get(), 4))
+                    command= lambda: find_all_subjects(dropdown1.get(),listy))
     button.place(relx=0.65, rely=0.75, anchor=CENTER)
-
-# @check_contracts
-def create_dropdown(window):
-    """This function will create a drop-down menu where you can select multiple options."""
-    # create a list of all the courses
-    courses = read_packet_csv()
-    # create a drop-down menu
-    dropdown = ttk.Combobox(window, values=courses, state="readonly")
-    # move it to bottom
-    dropdown.place(relx=0.5, rely=0.3, anchor=CENTER)
 
 
 # @check_contracts
@@ -103,12 +75,10 @@ def print_list(listy, course, window):
 
 def print_focus(fox: str, window):
     """This function will display the list of courses selected by the drop-down menu."""
-    global courses
     if fox != '':
         focy = fox
         label = Label(window, text=str(focy), bg="#282634", fg="white", font=("Helvetica", 10))
         label.place(relx=0.5, rely=0.28, anchor=CENTER)
-        print(courses)
 
 
 label = Label(root, text="Welcome to FocusForge", bg="#282634", fg="white", font=("Helvetica", 12))
@@ -141,6 +111,7 @@ def read_packet_csv(csv_file: str) -> list:
                 packets.append(network[0])
     return packets
 
+
 def read_give_names() -> list:
     """Read the given csv file and return a list of the first item in each row.
     """
@@ -152,7 +123,7 @@ def find_all_subjects(focus_name: str, completed: set):
 
     graph = CourseGraph.Graph()
     courses = [stry for stry in focus.setup_minimal_focii('focus-data.csv') if stry.name == focus_name][0]
-    focus.complete_minimal_focus(graph, courses, 'focus-data.csv')
+    focus.complete_minimal_focus(graph,courses,'focus-data.csv')
     diff_paths = courses.get_paths(completed)
     subjects_per_sem = CourseGraph.get_schedule(diff_paths[0], completed)
 
@@ -185,4 +156,3 @@ def find_all_subjects(focus_name: str, completed: set):
     tree.pack()
 
 mainloop()
-
